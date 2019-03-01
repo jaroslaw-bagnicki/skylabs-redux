c63a63f498431fae5033b0d763a7be99dff19c7d
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-01 18:54:07 +0100

    Add handling add comment in CommentsList component

--- src/components/CommentsList/index.js
+++ src/components/CommentsList/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { addComment } from '../../store/actions/commentActions';
+
 import Comment from '../Comment';
 
-const CommentList = ({ comments }) => (
-  <ul>
-    { comments.map(comment => (
-      <Comment key={comment.id} {...comment} />
-    ))}
-  </ul>
+const CommentList = ({ comments, handleAddComment }) => (
+  <>
+    <button onClick={handleAddComment}>Add comment</button>
+    <ul>
+      { comments.map(comment => (
+        <Comment key={comment.id} {...comment} />
+      ))}
+    </ul>
+  </>
 );
 
 const mapStateToProps = (state) => ({
   comments: state.comments
-})
+});
 
-export default connect(mapStateToProps)(CommentList);
+const mapDispatchToProps = (dispatch) => ({
+  handleAddComment: () => dispatch(addComment(prompt()))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentList);
