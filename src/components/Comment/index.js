6c4ec339c84ef775ba3acfbc667d5142e3305622
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-01 18:08:49 +0100

    Refacto Comment component

--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default ({text, vote}) => (
+import { voteUpComment } from '../../store/actions/commentActions';
+
+const Comment = ({ id, text, votes, voteUpComment }) => (
   <li>
-    {text} <span>votes: {vote}</span>
+    {text} <span>votes: {votes}</span>
+    <button onClick={() => voteUpComment(id)}>Vote up</button>
   </li>
 )
+
+export default connect(null, {voteUpComment})(Comment);
+
+
