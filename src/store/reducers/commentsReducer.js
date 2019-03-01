8ca47f230abc63c07717d6f622a3825a3a2d72cc
Jarosław Bagnicki <jarek@bagnicki.net>
2019-03-01 16:58:30 +0100

    Refactor comments reducer

--- src/store/reducers/commentsReducer.js
+++ src/store/reducers/commentsReducer.js
@@ -6,61 +6,49 @@ import {
   VOTE_DOWN_COMMENT
 } from '../reducers/comments';
 
-const initialState = {
-  comments: [],
-  users: []
-}
+const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case ADD_COMMENT:
-      return {
-        ...state,
-        commets: [
-          ...state.comments,
+
+      return [
+          ...state,
           {
             ...action.payload,
             vote: 0
           }
         ]
-      }
 
     case REMOVE_COMMENT:
-      return {
-        ...state,
-        comments: state.comments.filter(comment => comment.id !== action.payload.id)
-      }
-
+      return [
+        ...state.comments.filter(comment => comment.id !== action.payload.id)
+      ]
       
     case EDIT_COMMENT:
-      return {
-        ...state,
-        comments: state.comments.map(comment => {
+      return [
+        ...state.comments.map(comment => {
           if (comment.id === action.payload.id) comment.text = action.payload.text;
           return comment;
         })
-      }
-
+      ]
     case VOTE_UP_COMMENT:
-      return {
-        ...state,
-        comments: state.comments.map(comment => {
+      return [
+        ...state.comments.map(comment => {
           if (comment.id === action.payload.id) comment.vote = comment.vote + 1;
           return comment;
         })
-      }
+      ]
 
     case VOTE_DOWN_COMMENT:
-      return {
-        ...state,
-        comments: state.comments.map(comment => {
+      return [
+        ...state.comments.map(comment => {
           if (comment.id === action.payload.id) comment.vote = comment.vote - 1;
           return comment;
         })
-      }
+      ]
 
     default:
       return state;
   }
-
 }
